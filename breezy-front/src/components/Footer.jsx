533166cb28a383a8a87f1d9e669a135241bd7dc4
KazeRequiem <baptiste.noisette12@gmail.com>
2026-06-11 11:25:37 +0200

    fix: remove older components

--- breezy-front/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-    return (
-        <footer className="text-center text-xs text-gray-400 pb-6">
-            © 2026 Breezy — Tous droits réservés
-        </footer>
-    )
-}
-
-export default Footer
