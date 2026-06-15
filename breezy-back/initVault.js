8ea3a247aee8c7ab607eff8740f3353064220557
Floraaa <150044772+LonelyBadeline@users.noreply.github.com>
2026-06-15 15:35:08 +0200

    fix : fixing vault and docker overall

--- breezy-back/initVault.js
+++ breezy-back/initVault.js
@@ -1,19 +1,47 @@
-require("dotenv").config();
 const vault = require("node-vault")({
-    endpoint: process.env.VAULT_ADDR,
-    token: process.env.VAULT_TOKEN,
+  endpoint: process.env.VAULT_ADDR,
+  token: process.env.VAULT_TOKEN,
 });
 
-(async () => {
-    try {
+async function wait(ms) {
+  return new Promise((r) => setTimeout(r, ms));
+}
 
-        const secret = require("crypto").randomBytes(48).toString("hex");
-        await vault.write("secret/data/breezy", {
-            data: { jwt_secret: secret },
-        });
-        console.log("JWT_SECRET écrit en vault");
-    } catch (err){
-        console.error("Erreur init du vault", err.message);
-        process.exit(1);
+(async () => {
+  let ready = false;
+  for (let i = 0; i < 10; i++) {
+    try {
+      await vault.health();
+      ready = true;
+      break;
+    } catch (e) {
+      console.log(`Vault pas encore prêt, tentative ${i + 1}/10...`);
+      await wait(2000);
     }
+  }
+
+  if (!ready) {
+    console.error("Vault injoignable après 10 tentatives");
+    process.exit(1);
+  }
+  try {
+    try {
+      await vault.read("secret/data/breezy");
+      console.log("Secret déjà présent dans Vault, rien à faire.");
+      return;
+    } catch (e) {
+        console.log("Secret non trouvé, création en cours")
+    }
+
+    const secret =
+      process.env.JWT_SECRET || require("crypto").randomBytes(48).toString("hex");
+
+    await vault.write("secret/data/breezy", {
+      data: { jwt_secret: secret },
+    });
+    console.log("JWT_SECRET écrit dans Vault");
+  } catch (err) {
+    console.error("Erreur init Vault:", err.message);
+    process.exit(1);
+  }
 })();
--- breezy-back/routes/message.js
+++ breezy-back/routes/message.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const messageController = require("../controller/messageController");
+const messageController = require("../controllers/messageController");
 const verifyToken = require("../middlewares/auth");
 
 router.post("/", verifyToken, messageController.create);
