fbff0f403d9d870ee178a2b49bb334ce7331c6b3
Floraaa <150044772+LonelyBadeline@users.noreply.github.com>
2026-06-16 15:52:52 +0200

    Refacto: updating old index.js to app.js and server.js and changing swagger.js + adding routes/index.js

--- breezy-back/src/app.js
+++ breezy-back/src/app.js
@@ -1,40 +1,15 @@
-require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const swaggerUi = require("swagger-ui-express");
-const swaggerSpec = require("./swagger");
-const db = require("./models");
-const { loadSecrets } = require("./config/secrets");
+const swaggerSpec = require("./config/swagger");
+const env = require("./config/env");
 
 const app = express();
 
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true,
-}));
-
+app.use(cors({ origin: env.corsOrigin, credentials: true }));
 app.use(express.json());
-
-// Doc of the swagger
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.get("/", (req, res) => res.json({ message: "Breezy API up" }));
+app.use("/api", require("./routes"));
 
-app.get("/", (req, res) => res.json({ message: "Breezy API up"}));
-
-const PORT = process.env.APP_PORT || 3000;
-
-async function start() {
-  try{
-    await loadSecrets();
-    await db.sequelize.sync({ alter: true });
-    console.log("DB Synchronisée");
-
-    app.use("/api/auth", require("./routes/auth"));
-    app.use("/api/messages", require("./routes/message"));
-
-    app.listen(PORT, () => console.log(`Serveur sur port :  ${PORT}`));
-  } catch(err) {
-    console.error("Erreur au démarrage", err.message);
-    process.exit(1);
-  }
-}
-start();
+module.exports = app;
